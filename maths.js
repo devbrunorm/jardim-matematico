8b4a4ff19d5d13851f27cf981ec8597aa7a6fe40
Brunorm96 <59711742+Brunorm96@users.noreply.github.com>
2020-02-29 01:09:32 -0300

    Update maths.js

--- maths.js
+++ maths.js
@@ -13,11 +13,11 @@ function nextQuestion() {
 
 function checkAnswer() {
     const prediction = predictImage();
-    console.log(`Answer: ${answer}, Prediction: ${prediction}`);
+    console.log(`Answer: ${answer}, Previsão: ${prediction}`);
 
     if (prediction == answer) {
         score++;
-        console.log(`Correct! Score: ${score}`);
+        console.log(`Correto! Pontuação: ${score}`);
         if (score <= 6) {
             backgroundImages.push(`url('images/background${score}.svg')`);
             document.body.style.backgroundImage = backgroundImages;
@@ -29,7 +29,7 @@ function checkAnswer() {
         }
     } else {
         if (score != 0) { score--; }
-        console.log(`Wrong! Score: ${score}`);
+        console.log(`Errado! Pontuação: ${score}`);
         alert('Oops! Confira seus dados e tente escrever o número de uma forma mais clara desta vez...')
         setTimeout(function () {
             backgroundImages.pop();
